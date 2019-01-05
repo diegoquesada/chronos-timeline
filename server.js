308385fe1b61736788714c779e963ac24b2a3934
Diego Quesada <diego.developer@me.com>
2019-01-05 14:26:50 -0500

    - Added ejs and json-loader modules - Rolled back babel-loader to v7 - Basic basic server implementation and templates - Added about.html

---
+++ server.js
@@ -0,0 +1,18 @@
+import config from './config';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (reg, res) => {
+    res.render('index', {
+        content: 'Hello Express and <em>EJS</em>'
+    });
+});
+
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+    console.info('Express listening on port', config.port);
+});
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import data from './testData.json';
+
+ReactDOM.render(
+    React.createElement('h2', null, 'Hello Chronos!'),
+     document.getElementById('root')
+);
